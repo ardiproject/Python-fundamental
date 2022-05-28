30a6bab92af26c84e2f615ee2e7a9b1aa12251f5
ardiproject <ardipurnomobbm@gmail.com>
2022-05-28 20:10:33 +0700

    Perulangan FOR dan WHILE

--- dasar_sintaksis_perulangan_dengan_WHILE.py
+++ dasar_sintaksis_perulangan_dengan_WHILE.py
@@ -7,10 +7,9 @@ print('ibu berkata,"baca semua bukumu"')
 jumlah_buku_yang_sudah_dibaca = 0
 print(f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
 
-while jumlah_buku_yang_sudah_dibaca < jumlah_buku :
+while jumlah_buku_yang_sudah_dibaca < jumlah_buku:
     jumlah_buku_yang_sudah_dibaca = jumlah_buku_yang_sudah_dibaca + 1
     print(f"buku ke {jumlah_buku_yang_sudah_dibaca}")
 
 
 print(f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
-
